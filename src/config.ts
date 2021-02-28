0e9df8809cc89792a70e6a085cac2ebc46dbbb99
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-02-28 14:29:18 -0500

    more updating

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,18 @@
 import Phaser from "phaser";
+import GameScene from "./scenes/Game";
 
-export default {
+const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: "game",
-  backgroundColor: "#33A5E7",
+  backgroundColor: "#000000",
+  audio: { noAudio: true },
   scale: {
     width: 1920,
     height: 1080,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
+  scene: [GameScene],
 };
+
+export default config;
